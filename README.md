df740c939f36d79a8b2d047754d88ed15fe45c85
Myles Hathcock <myles@uaxe.(none)>
2012-11-04 17:24:46 -0600

    Added ignore, base file for install script

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-gitlab-ubuntu
-=============
+# gitlab-ubuntu
+Collection of installation utilities and (eventually) deb packages for GitLab on Ubuntu.
 
-Installation and support for GitLab on Ubuntu
+## Project Overview
+
+_gitlab-ubuntu_ aims to first provide a functional and featured install script that works out of the box on Ubuntu Server 12.04 LTS. 
+
+## Source
+
+In the ```source``` dir: contains installation scripts, and makefiles for creating deb packages.
+
+## Tests
+
+In the ```test``` dir: _coming soon_
---
+++ source/install-stable.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+###########################################
+##
+##	The standard sh shell is used
+##	(dash on ubuntu) because it's
+##	the closest to a posix-compliant
+##	shell that ships with ubuntu
+##	by default. 
+##
+###########################################
+
+###########################################
+##
+##	CONFIGURATION OPTIONS
+##
+###########################################
+
+
+
+###########################################
+##
+##	MAIN
+##
+###########################################
+
+echo "Installing gitlab..."
