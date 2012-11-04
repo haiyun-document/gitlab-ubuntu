7253fb98f938b3763045b566376b9add809760f1
Myles Hathcock <myles@uaxe.(none)>
2012-11-04 17:38:29 -0600

    Working from the install script on gitlab-recipes/install/debian_ubuntu.sh

--- source/install-stable.sh
+++ source/install-stable.sh
@@ -15,7 +15,10 @@
 ##
 ###########################################
 
-
+$REQUIRED_PACKAGES="git git-core gcc libxml2-dev libxslt-dev sqlite3 sqlite3-dev \
+	libcurl4-openssl-dev libreadline6-dev libc6-dev libssl-dev make build-essential \
+	zlib1g-dev libicu-dev redis-server openssh-server python-dev python-pip libyaml-dev \
+	postfix ruby1.9.3"
 
 ###########################################
 ##
@@ -23,4 +26,47 @@
 ##
 ###########################################
 
-echo "Installing gitlab..."
+#----------- Ensure Root Access -----------
+
+if [ "$(id -u)" != "0" ];
+then
+	echo "Must be run as root. Exiting." 
+	exit 1
+fi
+
+#----------- Install Prerequisites -----------
+
+apt-get install $REQUIRED_PACKAGES
+
+#----------- Add users -----------
+
+adduser --system --shell /bin/sh --gecos 'git user' --group \
+	--disabled-password --home /home/git git
+	
+adduser --disabled-login --gecos 'gitlab system user' gitlab
+
+#----------- Add users to eachother's groups -----------
+
+usermod -a -G git gitlab
+usermod -a -G gitlab git
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
