fdd06703d49f4e408213573dac8e920a50876086
Myles Hathcock <myles@uaxe.(none)>
2012-11-04 17:57:13 -0600

    adding more commands, ready to run and test

--- source/install-stable.sh
+++ source/install-stable.sh
@@ -15,10 +15,12 @@
 ##
 ###########################################
 
-$REQUIRED_PACKAGES="git git-core gcc libxml2-dev libxslt-dev sqlite3 sqlite3-dev \
+REQUIRED_PACKAGES="git git-core gcc libxml2-dev libxslt-dev sqlite3 sqlite3-dev \
 	libcurl4-openssl-dev libreadline6-dev libc6-dev libssl-dev make build-essential \
 	zlib1g-dev libicu-dev redis-server openssh-server python-dev python-pip libyaml-dev \
 	postfix ruby1.9.3"
+
+CHUSR="sudo -H -u"
 
 ###########################################
 ##
@@ -50,9 +52,17 @@ adduser --disabled-login --gecos 'gitlab system user' gitlab
 usermod -a -G git gitlab
 usermod -a -G gitlab git
 
+#----------- Generate RSA key ------------
 
+$CHUSR gitlab ssh-keygen -q -N '' -t rsa -f /home/gitlab/.ssh/id_rsa
 
+#----------- Install and configure Gitolite -----------
 
+# NOTE: This uses the fork of gitolite tweaked for GitLab
+
+cd /home/git
+$CHUSR git mkdir bin
+$CHUSR git git clone -b gl-v304 https://github.com/gitlabhq/gitolite.git gitolite-src
 
 
 
