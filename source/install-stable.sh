5e488ab693de1f1f55fa3472b8e59748cf697f14
Myles Hathcock <myles@uaxe.(none)>
2012-11-04 18:28:05 -0600

    Finished initial followthrough of the gilabhq install script

--- source/install-stable.sh
+++ source/install-stable.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-###########################################
+#############################################################
 ##
 ##	The standard sh shell is used
 ##	(dash on ubuntu) because it's
@@ -7,26 +7,24 @@
 ##	shell that ships with ubuntu
 ##	by default. 
 ##
-###########################################
+#############################################################
 
-###########################################
+#############################################################
 ##
 ##	CONFIGURATION OPTIONS
 ##
-###########################################
+#############################################################
 
 REQUIRED_PACKAGES="git git-core gcc libxml2-dev libxslt-dev sqlite3 libsqlite3-dev \
 	libcurl4-openssl-dev libreadline6-dev libc6-dev libssl-dev make build-essential \
 	zlib1g-dev libicu-dev redis-server openssh-server python-dev python-pip libyaml-dev \
 	postfix ruby1.9.3"
 
-CHUSR="sudo -H -u"
-
-###########################################
+#############################################################
 ##
 ##	MAIN
 ##
-###########################################
+#############################################################
 
 #----------- Ensure Root Access -----------
 
@@ -38,7 +36,7 @@ fi
 
 #----------- Install Prerequisites -----------
 
-apt-get install $REQUIRED_PACKAGES
+apt-get install -y $REQUIRED_PACKAGES
 
 #----------- Add users -----------
 
@@ -56,20 +54,33 @@ usermod -a -G gitlab git
 
 $CHUSR gitlab ssh-keygen -q -N '' -t rsa -f /home/gitlab/.ssh/id_rsa
 
+cp /home/gitlab/.ssh/id_rsa.pub /home/git/gitlab.pub
+chmod 0444 /home/git/gitlab.pub
+
 #----------- Install and configure Gitolite -----------
 
 # NOTE: This uses the fork of gitolite tweaked for GitLab
 
 cd /home/git
-$CHUSR git mkdir bin
-$CHUSR git git clone -b gl-v304 https://github.com/gitlabhq/gitolite.git gitolite-src
 
+sudo su git
+mkdir bin
+git clone -b gl-v304 https://github.com/gitlabhq/gitolite.git gitolite-src
+echo "PATH=\$PATH:/home/git/bin" >> /home/git/.profile
+echo "export PATH" >> /home/git/.profile
+gitolite-src/install -ln /home/git/bin
+PATH=/home/git/bin:$PATH
+gitolite setup -pk /home/git/gitlab.pub
+exit
 
+chmod -R g+rwX /home/git/repositories/
+chown -R git:git /home/git/repositories/
 
+#----------- Test the Install -------------
 
-
-
-
+sudo su gitlab
+git clone git@localhost:gitolite-admin.git /tmp/gitolite-admin
+rm -rf /tmp/gitolite-admin
 
 
 
